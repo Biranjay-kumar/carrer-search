bf740e8524a5b03e994656bb660a99b1392b1992
biranjay kumar gupta <biranjaykumargupta@gmail.com>
2024-09-04 23:59:54 +0530

    remove all the vulgarities

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import CareerAdviceBlog from "./components/CarrerAdvise";
 import CategoryCarousel from "./components/CategoryCarousel";
 import FAQPage from "./components/FAQPage";
@@ -8,19 +7,8 @@ import LatestJobs from "./components/LatestJobs";
 import Navbar from "./components/shared/Navbar";
 import SuccessStories from "./components/successStories";
 import useGetAllJobs from "./hooks/useGetAllJobs";
-import axios from 'axios';
-const Home = () => {
-  const callApi = async () => {
-    const data = await axios.get(
-      "https://d3qhqrhe9asm02.cloudfront.net/api/brand/fetch/user"
-    );
-    console.log(data);
-    
-  };
 
-  useEffect(() => {
-    callApi();
-  }, []);
+const Home = () => {
   useGetAllJobs();
   return (
     <div>
