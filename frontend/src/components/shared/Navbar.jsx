d181926514dcc5eb94035aa9de2669cbe3b6f508
biranjay kumar gupta <biranjaykumargupta@gmail.com>
2024-09-05 16:46:10 +0530

    added the render url

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
                 <div className="flex items-center gap-4">
                   <div>
                     <h4 className="text-lg font-semibold text-gray-800">
-                     {user?.fullname}
+                      {user?.fullname}
                     </h4>
                     {/* <p className="text-gray-600">You are a good boy</p> */}
                   </div>
