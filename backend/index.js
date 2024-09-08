08d45a023448acfcaa9c11f0ed559e0e5872ef01
biranjay kumar gupta <biranjaykumargupta@gmail.com>
2024-09-08 16:52:20 +0530

    fixed typo

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     // origin:'http://localhost:5173',
-    origin:'https://carrer-search.onrender.com',
+    origin: ['https://carrer-search.onrender.com', 'https://carrer-search.vercel.app'],
     credentials:true
 }
 
